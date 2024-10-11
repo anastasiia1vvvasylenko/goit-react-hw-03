61943fed38d522264c77d61767a86ca85e4bfd06
AnastasiiaV <166251420+anastasiia1vvvasylenko@users.noreply.github.com>
2024-10-11 20:40:39 +0200

    р

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+import ContactForm from './components/ContactForm/ContactForm';
+import SearchBox from './components/SearchBox/SearchBox';
+import ContactList from './components/ContactList/ContactList';
+import Contact from './components/Contact/Contact';
+
+function App() {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList>
+        {contacts.map(contactItem => {
+          return (
+            <Contact
+              key={contactItem.id}
+              id={contactItem.id}
+              name={contactItem.name}
+              phone={contactItem.phone}
+            />
+          );
+        })}
+      </ContactList>
+    </div>
+  );
+}
+
+export default App;
